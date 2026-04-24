86dcd7e1fdab4b4f96305ab30899b115822c3b4b
ahref24 <marklique8@gmail.com>
2026-04-24 17:53:37 +0800

    implement font

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import signUp from "@/app/(auth)/sign-up";
 import {Link} from "expo-router";
 
 const SignIn = () => {
--- app/(tabs)/insights.tsx
+++ app/(tabs)/insights.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text } from 'react-native';
 import {styled} from "nativewind";
 import {SafeAreaView as RNSafeAreaView} from "react-native-safe-area-context";
 const SafeAreaView = styled(RNSafeAreaView);
